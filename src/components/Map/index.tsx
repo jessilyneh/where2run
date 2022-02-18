393efca706a0b4cd3d40cbd876e49cb53058e82b
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-18 15:33:49 -0300

    create redirect page to markers using router

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 type Place = {
@@ -14,28 +15,36 @@ export type MapProps = {
   places?: Place[];
 };
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[0, 0]}
-    zoom={3}
-    style={{ height: "100%", width: "100%" }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {places?.map(({ id, name, location }) => {
-      const { latitude, longitude } = location;
+const Map = ({ places }: MapProps) => {
+  const router = useRouter();
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {places?.map(({ id, name, location, slug }) => {
+        const { latitude, longitude } = location;
 
-      return (
-        <Marker
-          key={`place-${id}`}
-          position={[latitude, longitude]}
-          title={name}
-        />
-      );
-    })}
-  </MapContainer>
-);
+        return (
+          <Marker
+            key={`place-${id}`}
+            position={[latitude, longitude]}
+            title={name}
+            eventHandlers={{
+              click: () => {
+                router.push(`/place/${slug}`);
+              },
+            }}
+          />
+        );
+      })}
+    </MapContainer>
+  );
+};
 
 export default Map;
