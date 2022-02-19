ab45e2445b4622922103a4edbbfdbedc73cad85f
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-19 19:20:43 -0300

    optimizing images with next images

--- next.config.js
+++ next.config.js
@@ -8,3 +8,9 @@ module.exports = withPWA({
     disable: !isProd,
   },
 });
+
+module.exports = {
+  images: {
+    domains: ['media.graphcms.com'],
+  },
+}
--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import LinkWrapper from "components/LinkWrapper";
 import { WindowClose } from "@styled-icons/fa-solid/WindowClose";
 import * as S from "./styles";
+import { useRouter } from "next/router";
 
 type imageProps = {
   url: string;
@@ -11,7 +13,7 @@ export type PlaceTemplateProps = {
   place: {
     slug: string;
     name: string;
-    description: {
+    description?: {
       html: string;
     };
     gallery: imageProps[];
@@ -19,6 +21,10 @@ export type PlaceTemplateProps = {
 };
 
 export default function PlacesTemplate({ place }: PlaceTemplateProps) {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
   return (
     <>
       <LinkWrapper href={"/"}>
@@ -30,12 +36,19 @@ export default function PlacesTemplate({ place }: PlaceTemplateProps) {
           <S.Heading>{place.name}</S.Heading>
 
           <S.Body
-            dangerouslySetInnerHTML={{ __html: place.description.html }}
+            dangerouslySetInnerHTML={{ __html: place.description?.html || "" }}
           />
 
           <S.Gallery>
             {place.gallery.map((image, index) => (
-              <img key={`photo-${index}`} src={image.url} alt={place.name} />
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
             ))}
           </S.Gallery>
         </S.Container>
