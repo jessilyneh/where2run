86dbcef45ed7235fec4f696526407b11c8768e0e
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-21 19:33:22 -0300

    add custon markers and define min zoom

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, MapConsumer } from "react-leaflet";
+import L from "leaflet";
+
+import * as S from "./styles";
 
 type Place = {
   id: string;
@@ -33,33 +36,63 @@ const CustomTileLayer = () => {
   );
 };
 
+const markerIcon = new L.Icon({
+  iconUrl: "img/w2r-pin.png",
+  iconSize: [60, 60],
+  iconAnchor: [40, 50],
+  popupAnchor: [0, -30],
+});
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter();
   return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <CustomTileLayer />
+    <S.MapWrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        minZoom={3}
+        style={{ height: "100%", width: "100%" }}
+        maxBounds={[
+          [-180, 180],
+          [180, -180],
+        ]}
+      >
+        <MapConsumer>
+          {(map) => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth;
 
-      {places?.map(({ id, name, location, slug }) => {
-        const { latitude, longitude } = location;
+            if (width < 780) {
+              map.setMinZoom(2);
+            }
 
-        return (
-          <Marker
-            key={`place-${id}`}
-            position={[latitude, longitude]}
-            title={name}
-            eventHandlers={{
-              click: () => {
-                router.push(`/place/${slug}`);
-              },
-            }}
-          />
-        );
-      })}
-    </MapContainer>
+            return null;
+          }}
+        </MapConsumer>
+
+        <CustomTileLayer />
+
+        {places?.map(({ id, name, location, slug }) => {
+          const { latitude, longitude } = location;
+
+          return (
+            <Marker
+              key={`place-${id}`}
+              position={[latitude, longitude]}
+              icon={markerIcon}
+              title={name}
+              eventHandlers={{
+                click: () => {
+                  router.push(`/place/${slug}`);
+                },
+              }}
+            />
+          );
+        })}
+      </MapContainer>
+    </S.MapWrapper>
   );
 };
 
